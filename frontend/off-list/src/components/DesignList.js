003dbd7e6979d6b966ca2f26a2a3378f98553aa8
weiweishcn <weiweishcn@gmail.com>
2024-12-02 18:54:00 -0800

    Update DesignList.js

--- frontend/off-list/src/components/DesignList.js
+++ frontend/off-list/src/components/DesignList.js
@@ -55,7 +55,7 @@ const ImageModal = ({ images, currentIndex, onClose, onNext, onPrevious }) => {
 
         {/* Image */}
         <img 
-          src={`http://localhost:3001${images[currentIndex]}`}
+          src={`{apiUrl}${images[currentIndex]}`}
           alt="Enlarged view"
           className="max-h-[90vh] max-w-[90vw] object-contain"
           onClick={(e) => e.stopPropagation()}
@@ -113,7 +113,7 @@ const DesignCard = ({ design }) => {
         <div className="relative h-64">
           {design.images && design.images[currentImageIndex] && (
             <img 
-              src={`http://localhost:3001${design.images[currentImageIndex]}`}
+              src={`{apiUrl}${design.images[currentImageIndex]}`}
               alt={`${design.tag} design by ${design.designer}`}
               className="w-full h-full object-cover cursor-pointer"
               onClick={() => setIsModalOpen(true)}
