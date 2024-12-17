bced843081ca01376dffb93e33f4f170dd818772
weiweishcn <weiweishcn@gmail.com>
2024-12-16 16:46:06 -0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ app.use((req, res, next) => {
 });
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://165.232.131.137:3000', 'https://pencildogs.com'],
+  origin: ['http://localhost:3000', 'http://165.232.131.137:3000', 'https://pencildogs.com', 'http://139.59.97.222:3000'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
