a98ce45623070bfea211f6a081e67e8358930b82
weiweishcn <weiweishcn@gmail.com>
2025-01-24 19:55:27 -0800

    Update config.js

--- frontend/off-list/src/i18n/config.js
+++ frontend/off-list/src/i18n/config.js
@@ -223,9 +223,9 @@ const resources = {
       hero: {
         title: 'Your personal design team',
         subtitle: "Let our professional designers bring your clients' visions to life",
-        viewDesigns: 'View Designs',
-        viewDesignsService: 'View Design Services',
-        viewDesignsStyle: 'View Design Styles'
+        viewDesigns: 'View Designs Services',
+        viewDesignsService: 'View Design Styles',
+        viewDesignsStyle: 'View Design'
       },
 
       // Login Page
