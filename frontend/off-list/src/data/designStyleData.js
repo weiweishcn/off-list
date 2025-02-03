12fc00b7d48eb430696d677f4fd3062685b13411
tgbtyty <alexqin777@gmail.com>
2025-02-03 18:27:49 -0500

    added style picture links on create-design

--- frontend/off-list/src/data/designStyleData.js
+++ frontend/off-list/src/data/designStyleData.js
@@ -1,44 +1,50 @@
 export const designStyleData = {
-  "American": [
+  "Classical Modern": [
     {
-      "location": "https://designimages.sfo3.digitaloceanspaces.com/design1/01.jpeg"
+      "location": "https://designimages.sfo3.digitaloceanspaces.com/design1/02.jpeg"
+    },
+    {
+      "location": "https://designimages.sfo3.digitaloceanspaces.com/design1/12.jpeg"
+    },
+    {
+      "location": "https://designimages.sfo3.digitaloceanspaces.com/design4/11.jpeg"
+    }
+  ],
+  "Modern American": [
+    {
+      "location": "https://designimages.sfo3.digitaloceanspaces.com/design2/22.jpeg"
+    },
+    {
+      "location": "https://designimages.sfo3.digitaloceanspaces.com/design2/11.jpeg"
+    }
+  ],
+  "Classical Chinese": [
+    {
+      "location": "https://designimages.sfo3.digitaloceanspaces.com/design2/25.jpeg"
+    },
+    {
+      "location": "https://designimages.sfo3.digitaloceanspaces.com/design5/34.jpeg"
     }
   ],
   "European": [
     {
-      "location": "https://designimages.sfo3.digitaloceanspaces.com/design1/01.jpeg"
-    },
-    {
-      "location": "https://designimages.sfo3.digitaloceanspaces.com/design1/02.jpeg"
+      "location": "https://designimages.sfo3.digitaloceanspaces.com/design2/15.jpeg"
     }
   ],
-  "Modern": [
+  "Minimalist": [
     {
-      "location": "https://designimages.sfo3.digitaloceanspaces.com/design1/01.jpeg"
+      "location": "https://designimages.sfo3.digitaloceanspaces.com/design3/01.jpeg"
     },
     {
-      "location": "https://designimages.sfo3.digitaloceanspaces.com/design1/02.jpeg"
+      "location": "https://designimages.sfo3.digitaloceanspaces.com/design3/02.jpeg"
     }
   ],
-  "Minialist": [
+  "Modern Chinese": [
     {
-      "location": "https://designimages.sfo3.digitaloceanspaces.com/design1/01.jpeg"
-    }
-  ],
-  "Mid-Century Modern Stlye": [
-    {
-      "location": "https://designimages.sfo3.digitaloceanspaces.com/design1/01.jpeg"
+      "location": "https://designimages.sfo3.digitaloceanspaces.com/design5/12.jpeg"
     },
     {
-      "location": "https://designimages.sfo3.digitaloceanspaces.com/design1/02.jpeg"
-    }
-  ],
-  "Artsy": [
-    {
-      "location": "https://designimages.sfo3.digitaloceanspaces.com/design1/01.jpeg"
-    },
-    {
-      "location": "https://designimages.sfo3.digitaloceanspaces.com/design1/02.jpeg"
+      "location": "https://designimages.sfo3.digitaloceanspaces.com/design5/35.jpeg"
     }
   ]
 }
